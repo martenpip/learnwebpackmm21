fc39a384e5753d20b40fdb316f17eab98478ffde
Marten Pipenberg <MM-21-910@tptlive.ee>
2022-09-20 12:30:53 +0300

    add bulma

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
-import './style.scss';
-console.log('hello world!')
 
-import hello from './hello.js';
-hello.hello();
+import './style.scss';
+let name = 'marten';
+const pi = 3.14;
+
+let obj = {
+    name: 'asd',
+    age: 20,
+    pets: [
+        {
+            name: 'asd',
+        }
+    ],
+    hello1(){
+        //do someting cool
+    },
+    hello2: function(){
+        //do someting cool
+    },
+    hello3: () => {
+
+    }
+};
+
+let nums = [0,3,2,4,1,5,6,7,9,8];
+
+nums.forEach(function(value){
+    console.log(value);
+});
+
+nums.forEach((value, index) => console.log(value));
+
+let powers = nums.map(value => value*value);
+console.log(powers);
+
+let sum = nums.reduce((prev,cur) => prev+cur);
+console.log(powers);
+
+let leftover = 5 % 3;
+console.log(leftover)
+
+let even = nums.filter(value =>  value%2==0)
+console.log(even);
+let sort = nums.sort();
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgecssPlugin = require('purgecss-webpack-plugin');
 const path = require('path');
 const glob = require('glob');
 
+
 let htmlPlugins = [];
 
-let files = glob.sync('./src/views/*.html');
+let files = glob.sync('./src/views/*.twig');
 files.forEach(file => {
   let htmlPlugin = new HtmlWebpackPlugin({
-    filename: file.split('/').at(-1),
-    template: file
+    filename: file.split('/').at(-1).replace('twig','html'),
+    template: file,
   });
   htmlPlugins.push(htmlPlugin);
 });
 
-
+const PATHS = {
+  src: path.join(__dirname, 'src')
+}
 
 
 module.exports = {
@@ -40,10 +44,21 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
+      {
+        test: /\.twig$/,
+        use: {
+          loader: 'twig-loader',
+          options: {
+          },
+        }
+      }
     ],
   },
   plugins: [
     ...htmlPlugins,
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new PurgecssPlugin.PurgeCSSPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true })
+    })
   ],
 };
